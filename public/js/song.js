992a56e09e94e8ab845a66e5a3045621d5dab3ac
Melihcan Cilek <melihcanclk@hotmail.com>
2023-07-30 05:24:57 +0300

    fix(public/css/song.css): fixed 500px with text decoration ellipsis

--- public/js/song.js
+++ public/js/song.js
@@ -1,7 +1,7 @@
-const track_name = document.getElementById('track-name');
-const artist_name = document.getElementById('artist-name');
-const album_name = document.getElementById('album-name');
-const album_image = document.getElementById('album-image');
+const track_name = document.getElementsByClassName('track-name')[0];
+const artist_name = document.getElementsByClassName('artist-name')[0];
+const album_name = document.getElementsByClassName('album-name')[0];
+const album_image = document.getElementsByClassName('album-image')[0];
 
 const head_title = document.getElementById('head-title');
 
@@ -12,26 +12,31 @@ const getSong = async () => {
     const current_album_name = album_name.innerHTML;
     const current_album_image = album_image.src;
 
-    const response = await fetch('/current-playing-song').catch(err => {
-        console.log(err);
-    });
+    const response = await fetch('/current-playing-song');
     const data = await response.json();
     if (data && data.track_name && data.track_name !== current_track_name && data.artist_name !== current_artist_name && data.album_name !== current_album_name && data.album_image !== current_album_image) {
-        const song_container = document.getElementsByClassName('song-container');
-        // add song-container-animation class to song-container
-        song_container[0].classList.add('song-container-animation');
+        
+        track_name.innerHTML = data.track_name;
+        artist_name.innerHTML = data.artist_name;
+        album_name.innerHTML = data.album_name;
+        album_image.src = data.album_image;
 
-        // remove song-container-animation class from song-container after 1s
-        setTimeout(() => {
-            song_container[0].classList.remove('song-container-animation');
+        head_title.innerText = data.track_name + ' - ' + data.artist_name + ' ';
 
-            track_name.innerHTML = data.track_name;
-            artist_name.innerHTML = data.artist_name;
-            album_name.innerHTML = data.album_name;
-            album_image.src = data.album_image;
+        const animation = [
+            { transform: 'translateY(100%)' },
+            { transform: 'translateY(0)' }
+        ]
+        const animation_setting = {
+            duration: 1000,
+            easing: 'ease-in-out',
+            fill: 'forwards'
+        }
 
-            head_title.innerText = data.track_name + ' - ' + data.artist_name + ' ';
-        }, 500);
+        track_name.animate(animation, animation_setting);
+        artist_name.animate(animation, animation_setting);
+        album_name.animate(animation, animation_setting);
+        album_image.animate(animation, animation_setting);
 
     }
 }
