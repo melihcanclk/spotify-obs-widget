ce142d27f80e343ac2bb4ca7ae4c85e41a4236c6
Melihcan Cilek <melihcanclk@hotmail.com>
2023-08-17 20:38:28 +0300

    rollback(): animation cancel

--- public/js/song.js
+++ public/js/song.js
@@ -6,35 +6,6 @@ const song_border = document.getElementsByClassName('song-border')[0];
 
 const head_title = document.getElementById('head-title');
 
-const animation = [
-    { transform: 'translateY(100%)' },
-    { transform: 'translateY(0)' }
-]
-const animation_setting = {
-    duration: 1000,
-    easing: 'ease-in-out',
-    fill: 'forwards'
-}
-
-const animateComponent = (track_name, artist_name, album_name, album_image, isReverse) => {
-    if (isReverse) {
-        animation_setting.direction = 'reverse';
-    } else {
-        animation_setting.direction = 'normal';
-    }
-
-    track_name.animate(animation, animation_setting);
-    artist_name.animate(animation, animation_setting);
-    album_name.animate(animation, animation_setting);
-    album_image.animate(animation, animation_setting);
-}
-
-const checkIfSongChanged = (current_track_name, current_artist_name, current_album_name, current_album_image, data) => {
-    if (data.track_name !== current_track_name || data.artist_name !== current_artist_name || data.album_name !== current_album_name || data.album_image !== current_album_image) {
-        return true;
-    }
-    return false;
-}
 
 let isSongPlaying = false;
 
@@ -48,25 +19,22 @@ const getSong = async () => {
     const data = await response.json();
     if (data.is_playing) {
 
-        if (checkIfSongChanged(current_track_name, current_artist_name, current_album_name, current_album_image, data)) {
-            track_name.innerHTML = data.track_name;
-            artist_name.innerHTML = data.artist_name;
-            album_name.innerHTML = data.album_name;
-            album_image.src = data.album_image;
+        track_name.innerHTML = data.track_name;
+        artist_name.innerHTML = data.artist_name;
+        album_name.innerHTML = data.album_name;
+        album_image.src = data.album_image;
 
-            head_title.innerText = data.track_name + ' - ' + data.artist_name + ' ';
-            animateComponent(track_name, artist_name, album_name, album_image, false);
+        head_title.innerText = data.track_name + ' - ' + data.artist_name + ' ';
 
-            if (!isSongPlaying) {
-                isSongPlaying = true;
-                // remove div beside artist name
-                const divElement = document.getElementsByClassName('no-song-playing')[0];
-                if (divElement) {
-                    divElement.remove();
-                }
+        if (!isSongPlaying) {
+            isSongPlaying = true;
+            // remove div beside artist name
+            const divElement = document.getElementsByClassName('no-song-playing')[0];
+            if (divElement) {
+                divElement.remove();
             }
-
         }
+
     } else {
         const noSongPlaying = document.getElementsByClassName('no-song-playing')[0];
         if (noSongPlaying) {
@@ -83,7 +51,6 @@ const getSong = async () => {
             album_name.innerHTML = '';
             album_image.src = '';
             head_title.innerText = 'No song is playing';
-            animateComponent(track_name, artist_name, album_name, album_image, true);
             isSongPlaying = false;
         }
 
